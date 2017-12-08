6fd92355e738dfc14b02694d0c84900bfdee3a03
liuda <liuda@gpu5.adsys.zzzc.qihoo.net>
2017-12-08 19:12:16 +0800

    multi-coord with tf.Coord

--- tf_action/7_1_Word2Vec.py
+++ tf_action/7_1_Word2Vec.py
@@ -26,7 +26,10 @@ import tensorflow as tf
 
 # Step 1: Download the data.
 url = 'http://mattmahoney.net/dc/'
-local_data_path = "/Users/liuda/Local/data/"
+local_data_path = './data/'
+checkpoint_dir = './checkpoint_dir/'
+if not os.path.exists(checkpoint_dir):
+  os.mkdir(checkpoint_dir)
 
 def maybe_download(filename, expected_bytes):
   """Download a file if not present, and make sure it's the right size."""
@@ -43,6 +46,7 @@ def maybe_download(filename, expected_bytes):
   return filename
 
 filename = maybe_download('text8.zip', 31344016)
+#filename = maybe_download('centence.zip', 186)
 print("filename download")
 
 # Read the data into a list of strings.
@@ -90,9 +94,14 @@ print('Sample data', data[:10], [reverse_dictionary[i] for i in data[:10]])
 
 data_index = 0
 
+# print("data: ", data) # 'data: ', [4, 1, 5, 2, 3, 1]   词频由高到低在文中的顺序
 # Step 3: Function to generate a training batch for the skip-gram model.
 # batch:
 # labels:
+#  0   1  2  3  4   5
+# the dog is a big dog
+# batch: array([1, 1, 5, 5, 2, 2, 3, 3], dtype=int32)
+# label: array([[5], [4], [2], [1], [5], [3], [2], [1]], dtype=int32)
 def generate_batch(batch_size, num_skips, skip_window):
   global data_index
   assert batch_size % num_skips == 0
@@ -102,7 +111,7 @@ def generate_batch(batch_size, num_skips, skip_window):
   span = 2 * skip_window + 1 # [ skip_window target skip_window ]
   buffer = collections.deque(maxlen=span)   # deque([], maxlen = 3)   最大长度是3, 超过3时先进入的数据会自动被清空
   for _ in range(span):
-    buffer.append(data[data_index])
+    buffer.append(data[data_index])  # 第一个进buff的词是词频最高的,每次进3个
     data_index = (data_index + 1) % len(data)
   for i in range(batch_size // num_skips):
     target = skip_window  # target label at the center of the buffer
@@ -121,7 +130,6 @@ batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1)
 for i in range(8):
   print(batch[i], reverse_dictionary[batch[i]],
       '->', labels[i, 0], reverse_dictionary[labels[i, 0]])
-
 # Step 4: Build and train a skip-gram model.
 
 batch_size = 128
@@ -142,26 +150,27 @@ graph = tf.Graph()
 with graph.as_default():
 
   # Input data.
-  train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+  train_inputs = tf.placeholder(tf.int32, shape=[batch_size])   #inputs -> embedding
   train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
   valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
 
   # Ops and variables pinned to the CPU because of missing GPU implementation
-  with tf.device('/cpu:0'):
-    # Look up embeddings for inputs.
-    embeddings = tf.Variable(
-        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
-    embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+  #with tf.device('/cpu:0'):
+  # Look up embeddings for inputs.
+  embeddings = tf.Variable(
+      tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+  embed = tf.nn.embedding_lookup(embeddings, train_inputs)
 
-    # Construct the variables for the NCE loss
-    nce_weights = tf.Variable(
-        tf.truncated_normal([vocabulary_size, embedding_size],
-                            stddev=1.0 / math.sqrt(embedding_size)))
-    nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+  # Construct the variables for the NCE loss
+  nce_weights = tf.Variable(
+      tf.truncated_normal([vocabulary_size, embedding_size],
+                          stddev=1.0 / math.sqrt(embedding_size)))
+  nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
 
   # Compute the average NCE loss for the batch.
   # tf.nce_loss automatically draws a new sample of the negative labels each
   # time we evaluate the loss.
+  # tf.nn.nce_loss: 
   loss = tf.reduce_mean(
       tf.nn.nce_loss(weights=nce_weights,
                      biases=nce_biases,
@@ -172,6 +181,7 @@ with graph.as_default():
 
   # Construct the SGD optimizer using a learning rate of 1.0.
   optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+  saver = tf.train.Saver(max_to_keep=4, keep_checkpoint_every_n_hours=2)
 
   # Compute the cosine similarity between minibatch examples and all embeddings.
   norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
@@ -187,42 +197,57 @@ with graph.as_default():
 # Step 5: Begin training.
 num_steps = 100001
 
-with tf.Session(graph=graph) as session:
-  # We must initialize all variables before we use them.
-  init.run()
-  print("Initialized")
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+with tf.Session(graph=graph, config = config ) as session:
+  ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+  if ckpt and ckpt.model_checkpoint_path:
+      saver.restore(session, ckpt.model_checkpoint_path)
+      print("Restore from %s" % ckpt.model_checkpoint_path)
+  else:
+      # We must initialize all variables before we use them.
+      init.run()
+      print("Initialized")
 
   average_loss = 0
-  for step in range(num_steps):
-    batch_inputs, batch_labels = generate_batch(
-        batch_size, num_skips, skip_window)
-    feed_dict = {train_inputs : batch_inputs, train_labels : batch_labels}
+  coord = tf.train.Coordinator()
+  threads = tf.train.start_queue_runners(sess = session, coord = coord)
+  try:
+      for step in range(num_steps):
+        batch_inputs, batch_labels = generate_batch(
+            batch_size, num_skips, skip_window)
+        feed_dict = {train_inputs : batch_inputs, train_labels : batch_labels}
 
-    # We perform one update step by evaluating the optimizer op (including it
-    # in the list of returned values for session.run()
-    _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
-    average_loss += loss_val
+        # We perform one update step by evaluating the optimizer op (including it
+        # in the list of returned values for session.run()
+        _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
+        average_loss += loss_val
 
-    if step % 2000 == 0:
-      if step > 0:
-        average_loss /= 2000
-      # The average loss is an estimate of the loss over the last 2000 batches.
-      print("Average loss at step ", step, ": ", average_loss)
-      average_loss = 0
+        if step % 2000 == 0:
+          if step > 0:
+            average_loss /= 2000
+          # The average loss is an estimate of the loss over the last 2000 batches.
+          print("Average loss at step ", step, ": ", average_loss)
+          average_loss = 0
 
-    # Note that this is expensive (~20% slowdown if computed every 500 steps)
-    if step % 10000 == 0:
-      sim = similarity.eval()
-      for i in range(valid_size):
-        valid_word = reverse_dictionary[valid_examples[i]]
-        top_k = 8 # number of nearest neighbors
-        nearest = (-sim[i, :]).argsort()[1:top_k+1]
-        log_str = "Nearest to %s:" % valid_word
-        for k in range(top_k):
-          close_word = reverse_dictionary[nearest[k]]
-          log_str = "%s %s," % (log_str, close_word)
-        print(log_str)
-  final_embeddings = normalized_embeddings.eval()
+        # Note that this is expensive (~20% slowdown if computed every 500 steps)
+        if step % 10000 == 0:
+          saver.save(session,checkpoint_dir + '/model.ckpt')
+          sim = similarity.eval()
+          for i in range(valid_size):
+            valid_word = reverse_dictionary[valid_examples[i]]
+            top_k = 8 # number of nearest neighbors
+            nearest = (-sim[i, :]).argsort()[1:top_k+1]
+            log_str = "Nearest to %s:" % valid_word
+            for k in range(top_k):
+              close_word = reverse_dictionary[nearest[k]]
+              log_str = "%s %s," % (log_str, close_word)
+            print(log_str)
+      final_embeddings = normalized_embeddings.eval()
+  except tf.errors.OutOfRangeError:
+    print("done")
+  coord.request_stop()
+  coord.join(threads)
 
 # Step 6: Visualize the embeddings.
 
